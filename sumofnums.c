5ac6fecd33de663d0dc5b82def1461dec99b924c
afeefaa333 <afeefaa333@gmail.com>
2020-10-04 22:33:37 +0530

    Corrected the program to find sum

--- sumofnums.c
+++ sumofnums.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 void main(){
-int nums[5]={1,2,3,4,5};
-  int i,sum=0;
-for (i=0;i<5;i++)
-  sum+=i;
-  printf(sum);
+	int nums[5];
+	int i,sum=0;
+	printf("Enter five numbers:");
+	for(i=0;i<5;i++)
+		scanf("%d",&nums[i]);
+	for (i=0;i<5;i++)
+		sum+=nums[i];
+	printf("The sum is: %d",sum);
   
 }
+
